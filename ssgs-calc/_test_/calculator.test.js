4486f0faf0e8fc1ed99f594b7ce00000a8c0e0f9
DiegoVerrina <146079625+DiegoVerrina@users.noreply.github.com>
2025-04-03 22:50:57 +0200

    Update calculator.test.js

--- ssgs-calc/_test_/calculator.test.js
+++ ssgs-calc/_test_/calculator.test.js
@@ -6,8 +6,8 @@ test("Addizione corretta: 2 + 2 = 4", () => {
     expect(add(2, 2)).toBe(4); // Test che dovrebbe passare
   });
   
-  test("Test corretto: 3 + 5 = 8", () => {
-    expect(add(3, 5)).toBe(8); // Test che passa (perché il risultato è 8)
+  test("Test errato: 3 + 5 = 7", () => {
+    expect(add(3, 5)).toBe(7); // Test che non passa (perché il risultato è 8)
   });
 
     test("Sottrazione corretta: 5 - 3 = 2", () => {
